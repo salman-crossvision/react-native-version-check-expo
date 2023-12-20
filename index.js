ba4514118f73ebe9f67a4b22a343a244b7d55969
salman-crossvision <135328240+salman-crossvision@users.noreply.github.com>
2023-12-20 17:59:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 import builder from 'react-native-version-check/builder';
 import VersionInfo from './src/ExpoVersionInfo';
 
-export default function(country){
-	return builder(VersionInfo(country));
+export default function(){
+	return builder(VersionInfo("US"));
 };
